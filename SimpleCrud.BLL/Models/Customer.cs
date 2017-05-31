7636d08707ea0edd623d3eed6c6fd5daa38260fe
Simon Skrabanek <szymon.skrabanek@gmail.com>
2017-05-31 16:01:58 +0200

    Created initial solution with front-end, BLL and DAL projects. Added simple Customer domain model

---
+++ SimpleCrud.BLL/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimpleCrud.BLL.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
